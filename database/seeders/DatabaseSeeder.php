d8b066f69a47894406260a3f5c77a17131f1c56f
dioproject <91179318+dioproject@users.noreply.github.com>
2024-06-17 18:50:27 +0700

    fixL database seeder and now done run sll seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            GenderSeeder::class,
+            ReligionSeeder::class,
+            StatusSeeder::class,
+        ]);
+    }
+}
