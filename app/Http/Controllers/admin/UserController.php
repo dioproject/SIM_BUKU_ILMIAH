a092be5dcbb38693c2176954ca21a17754295af8
dioproject <gagalginjal9@gmail.com>
2024-07-23 06:12:01 +0700

    fix: users menu

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -7,117 +7,91 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Gender;
-use App\Models\Religion;
 use App\Models\History;
 
 class UserController extends Controller
 {
     public function index(Request $request)
     {
-
         $search = $request->input('search');
         if ($search) {
-            $users = User::where('first_name', 'LIKE', '%' . $search . '%')
-                         ->orWhere('last_name', 'LIKE', '%' . $search . '%')
-                         ->paginate(10); 
+            $users = User::where('username', 'LIKE', '%' . $search . '%')->paginate(5);
         } else {
-        $users = User::paginate(10);
+            $users = User::paginate(5);
         }
         return view('pages.admin.users.index', compact('users'));
     }
 
     public function create()
     {
-        $gender = Gender::all();
-        $religion = Religion::all();
-
-        return view('pages.admin.users.create', compact('gender', 'religion'));
+        return view('pages.admin.users.create');
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'first_name' => 'required|max:30',
-            'last_name' => 'required|max:100',
+            'username' => 'required|max:30',
+            'name' => 'required|max:100',
             'email' => 'required|email|max:50|unique:users',
             'password' => 'required|min:8',
-            'religion' => 'required',
-            'gender' => 'required',
-            'place_of_birth' => 'required|max:100',
-            'date_of_birth' => 'required|date',
             'contact' => 'required|max:30',
             'user_role' => 'required|in:ADMIN,EDITOR,AUTHOR',
         ]);
 
         $user = User::create([
+            'username' => $request->username,
+            'name' => $request->name,
             'email' => $request->email,
+            'contact' => $request->contact,
             'password' => Hash::make($request->password),
             'user_role' => $request->user_role,
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'place_of_birth' => $request->place_of_birth,
-            'date_of_birth' => $request->date_of_birth,
-            'contact' => $request->contact,
-            'religion_id' => $request->religion,
-            'gender_id' => $request->gender,
         ]);
 
         if ($user) {
-
             History::create([
-                'change_detail' => Auth::user()->first_name . ' created user ' . $user->first_name . ' successfully.',
-                'user_id' => Auth::id(),
+                'change_detail' => Auth::user()->username . ' added user ' . $user->username,
             ]);
-            return redirect()->route('admin.index.user')->with('success', Auth::user()->first_name . ' created user ' . $user->first_name . ' successfully.');
+            return redirect()->route('admin.index.user')->with('success', Auth::user()->username . ' added user ' . $user->username . ' successfully.');
         }
+        return redirect()->route('admin.create.book')->with('error', 'User not found.');
     }
 
     public function edit($id)
     {
         $user = User::findOrFail($id);
-        $gender = Gender::all();
-        $religion = Religion::all();
 
-        return view('pages.admin.users.edit', compact('user', 'gender', 'religion'));
+        return view('pages.admin.users.edit', compact('user'));
     }
 
     public function update(Request $request, $id)
     {
         $request->validate([
-            'first_name' => 'required|max:30',
-            'last_name' => 'required|max:100',
+            'username' => 'required|max:30',
+            'name' => 'required|max:100',
+            'email' => 'required|email|max:50|unique:users',
             'password' => 'required|min:8',
-            'religion' => 'required',
-            'gender' => 'required',
-            'place_of_birth' => 'required|max:100',
-            'date_of_birth' => 'required|date',
             'contact' => 'required|max:30',
             'user_role' => 'required|in:ADMIN,EDITOR,AUTHOR',
         ]);
 
         $user = User::findOrFail($id);
         $user->update([
+            'username' => $request->username,
+            'name' => $request->name,
+            'email' => $request->email,
+            'contact' => $request->contact,
             'password' => Hash::make($request->password),
             'user_role' => $request->user_role,
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'place_of_birth' => $request->place_of_birth,
-            'date_of_birth' => $request->date_of_birth,
-            'contact' => $request->contact,
-            'religion_id' => $request->religion,
-            'gender_id' => $request->gender,
-            'updated_at',
+            'updated_at' => now(),
         ]);
 
         if ($user) {
             History::create([
-                'change_detail' => Auth::user()->first_name . ' updated user ' . $user->first_name . ' successfully.',
-                'user_id' => Auth::id(),
+                'change_detail' => Auth::user()->username . ' updated user ' . $user->username,
             ]);
-            return redirect()->route('admin.index.user')->with('success', Auth::user()->first_name . ' updated user ' . $user->first_name . ' successfully.');
+            return redirect()->route('admin.index.user')->with('success', Auth::user()->username . ' updated user ' . $user->username . ' successfully.');
         }
-        return redirect()->route('admin.create.book')->with('error', 'User not found.');
+        return redirect()->route('admin.edit.book')->with('error', 'User not found.');
     }
 
     public function destroy($id)
@@ -127,11 +101,10 @@ class UserController extends Controller
 
         if ($user) {
             History::create([
-                'change_detail' => Auth::user()->first_name . ' deleted user ' . $user->first_name . ' successfully.',
-                'user_id' => Auth::id(),
+                'change_detail' => Auth::user()->username . ' deleted user ' . $user->username,
             ]);
             return redirect()->route('admin.index.user');
-        } 
+        }
         return redirect()->route('admin.index.user');
     }
 }
